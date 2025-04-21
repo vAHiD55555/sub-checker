30e16e24e2f232f1ebdcacfd3ba4a72c2332a5b6
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2025-04-21 21:27:16 +0330

    Update cl.py

--- cl.py
+++ cl.py
@@ -25,7 +25,7 @@ with open(CONF_PATH,"r") as file_client_set:
         f=json.load(file_client_set)
         test_link_=f["core"]["test_url"]
 TEXT_PATH="normal.txt"
-LINK_PATH="https://raw.githubusercontent.com/mrdani13/sub-checker/refs/heads/main/normal.txt"
+LINK_PATH=""
 FIN_PATH="final.txt"
 FIN_CONF=[]
 def remove_empty_strings(input_list):
