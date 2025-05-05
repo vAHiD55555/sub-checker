e50015e0e4533a99960496f553c76572aeb79fbe
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2025-05-05 10:33:11 +0330

    Update cl.py

--- cl.py
+++ cl.py
@@ -258,7 +258,7 @@ def parse_configs(conifg,num=0,cv=1,hy2_path="hy2/config.yaml",is_hy2=False): # 
             tag=tag,
             id=vmess_data.get("id", ""),
             alter_id=int(vmess_data.get("aid", 0)),
-            scy=vmess_data.get("scy", ""),
+            scy=vmess_data.get("scy", "auto"),
             sni=vmess_data.get("sni", ""),
             fp=vmess_data.get("fp", ""),
             type=vmess_data.get("net","tcp"),
@@ -1165,7 +1165,11 @@ def parse_configs(conifg,num=0,cv=1,hy2_path="hy2/config.yaml",is_hy2=False): # 
         if TYPE=="kcp":
             outboundBean_Stream_kcp_settings=V2rayConfig.OutboundBean.KcpSettingsBean(congestion=False,downlinkCapacity=100,header=V2rayConfig.OutboundBean.HeaderBean(type=HEADER_TYPE),mtu=1350,readBufferSize=1,seed=PATH,tti=50,uplinkCapacity=12,writeBufferSize=1)
         elif TYPE=="ws":
-            outboundBean_Stream_ws_settings=V2rayConfig.OutboundBean.WsSettingsBean(headers=V2rayConfig.OutboundBean.HeadersBean(Host=R_HOST,userAgent=None,acceptEncoding=None,Connection=None,Pragma=None,Host_single=None),path=PATH)
+            if SECURITY != "tls" and R_HOST:
+                ws_headers = V2rayConfig.OutboundBean.HeadersBean(Host=R_HOST)
+            else:
+                ws_headers=None
+            outboundBean_Stream_ws_settings=V2rayConfig.OutboundBean.WsSettingsBean(headers=ws_headers,path=PATH)
         elif TYPE=="httpupgrade":
             outboundBean_Stream_httpupgrade_settings=V2rayConfig.OutboundBean.HttpupgradeSettingsBean(host=R_HOST,path=PATH)
         elif TYPE=="xhttp":
